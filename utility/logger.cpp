995cca71a0aba4f19998f83c95ee4cc4d29a1202
腐海 <86948417+RottenSea@users.noreply.github.com>
2024-06-27 01:52:52 -0700

    Rewrite

--- utility/logger.cpp
+++ utility/logger.cpp
@@ -3,37 +3,6 @@
 
 #include "logger.h"
 
-std::ofstream Logger::m_error_log_file;
-std::ofstream Logger::m_info_log_file;
-std::ofstream Logger::m_warn_log_file;
-
-void initLogger(const std::string &info_log_filename,
-                const std::string &warn_log_filename,
-                const std::string &error_log_filename)
-{
-    Logger::m_info_log_file.open(info_log_filename.c_str());
-    Logger::m_warn_log_file.open(warn_log_filename.c_str());
-    Logger::m_error_log_file.open(error_log_filename.c_str());
-}
-
-std::ostream &Logger::start(level log_level,
-                            const int line,
-                            const std::string &function)
-{
-    time_t tm;
-    time(&tm);
-    char time_string[128];
-    time_t(&tm) = tm;
-    return getStream(log_rank) << time_string
-                               << "function (" << function << ")"
-                               << "line " << line
-                               << std::flush;
-}
-
-
-
-
-
 
 
 
--- utility/logger.h
+++ utility/logger.h
@@ -15,23 +15,7 @@ public:
         FATAL
     };
 
-    Logger(level log_level) : m_log_rank(log_level){};
-
-    ~Logger();
-
-    static std::ostream &start(level log_level,
-                               const int32_t line,
-                               const std::string &function);
-
-    friend void initLogger(const std::string &info_log_filename,
-                           const std::string &warn_log_filename,
-                           const std::string &error_log_filename);
 
 private:
-    static std::ostream &getStream(level log_level);
 
-    static std::ofstream m_info_log_file;
-    static std::ofstream m_warn_log_file;
-    static std::ofstream m_error_log_file;
-    level m_log_rank;
 };
