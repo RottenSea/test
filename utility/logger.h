3619aa595dc0f26fdf3ae8c9b3ea4b5f130e9003
腐海 <86948417+RottenSea@users.noreply.github.com>
2024-06-26 22:46:44 -0700

    How to log?

--- utility/logger.h
+++ utility/logger.h
@@ -3,22 +3,15 @@
 #include <iostream>
 #include <fstream>
 
-void initLogger(const std::string &info_log_filename,
-                const std::string &warn_log_filename,
-                const std::string &error_log_filename);
-
 class Logger
 {
-    friend void initLogger(const std::string &info_log_filename,
-                           const std::string &warn_log_filename,
-                           const std::string &error_log_filename);
-
 public:
-    Logger(level log_rank) : m_log_rank(log_rank){};
+    Logger(level log_level) : m_log_rank(log_level){};
+
     ~Logger();
+
     static std::ostream &start(level log_rank, const int32_t line, const std::string &function);
 
-private:
     enum level
     {
         DEBUG,
@@ -28,16 +21,15 @@ private:
         FATAL
     };
 
+    friend void initLogger(const std::string &info_log_filename,
+                           const std::string &warn_log_filename,
+                           const std::string &error_log_filename);
+
+private:
     static std::ostream &getStream(level log_rank);
 
     static std::ofstream m_info_log_file;
     static std::ofstream m_warn_log_file;
     static std::ofstream m_error_log_file;
-    log_rank_t m_log_rank;
-
-    static void initLogger(const std::string &info_log_filename,
-                           const std::string &warn_log_filename,
-                           const std::string &error_log_filename)
-    {
-    }
+    level m_log_rank;
 };
