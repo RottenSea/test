d41eea38e687e2fb384839e92da729315d147b76
腐海 <86948417+RottenSea@users.noreply.github.com>
2024-06-28 00:14:45 -0700

    arrangement

--- utility/logger.cpp
+++ utility/logger.cpp
@@ -2,56 +2,9 @@
 
 std::string message = "Hello, world!";
 
-void LogToFile()
-{
-    std::string folderpath = "logs";
-    if (std::filesystem::create_directory(folderpath))
-    {
-        std::cout << "文件夹创建成功：" << folderpath << std::endl;
-    }
-    else
-    {
-        std::cerr << "文件夹创建失败或文件夹已存在：" << folderpath << std::endl;
-    }
-
-    std::ofstream ofs("logs/log.log", std::ios::app);
-    if (!ofs.is_open())
-    {
-        std::cout << "无法打开日志文件" << std::endl;
-        return;
-    }
-    ofs << message << std::endl;
-    ofs.close();
-};
-
-std::string GetCurrentTime()
-{
-    std::time_t now = std::time(nullptr);
-
-    std::tm *local_now = std::localtime(&now);
-
-    char buffer[32];
-
-    std::strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", local_now);
-
-    std::cout << "当前时间: " << buffer << std::endl;
-
-    return buffer;
-}
-
-void Logger::Log(Level level, const std::string *message)
-{
-    LevelToString(level);
-
-    std::string filename = GetCurrentTime() + ".log";
-
-    std::string filepath = "logs/" + filename;
-
-    LogToFile();
-}
-
 int main()
 {
     std::cout << "Hello, world!" << std::endl;
-    LogToFile();
+    Logger::LogToFile();
 }
+
--- utility/logger.h
+++ utility/logger.h
@@ -18,7 +18,54 @@ public:
         Fatal
     };
 
+    std::string *message;
+    std::string *folderpath = "logs";
     static void Log(Level level, const std::string *message);
+
+    std::string GetCurrentTime()
+    {
+        std::time_t now = std::time(nullptr);
+
+        std::tm *local_now = std::localtime(&now);
+
+        char buffer[32];
+
+        std::strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", local_now);
+
+        return buffer;
+    }
+
+    static void LogToFile(std::string message, std::string folderpath)
+    {
+        if (std::filesystem::create_directory(folderpath))
+        {
+            std::cout << "folder created: " << folderpath << std::endl;
+        }
+        else
+        {
+            std::cerr << "Folder creation failure or already exists: " << folderpath << std::endl;
+        }
+
+        std::ofstream ofs("logs/log.log", std::ios::app);
+        if (!ofs.is_open())
+        {
+            std::cout << "无法打开日志文件" << std::endl;
+            return;
+        }
+        ofs << message << std::endl;
+        ofs.close();
+    };
+
+    void Logger::Log(Level level, const std::string *message)
+    {
+        LevelToString(level);
+
+        std::string filename = GetCurrentTime() + ".log";
+
+        std::string filepath = "logs/" + filename;
+
+        LogToFile(*message, *message);
+    }
 
 private:
     static std::string LevelToString(Level level)
