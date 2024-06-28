6c3f2fbc50f2d5916a81510e3484c8688b1ed8b3
腐海 <86948417+RottenSea@users.noreply.github.com>
2024-06-27 21:07:13 -0700

    go ahead

--- utility/logger.cpp
+++ utility/logger.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
+#include <ctime>
 
 #include "logger.h"
 
+int main()
+{
+    std::time_t now = std::time(nullptr);
 
+    std::tm* local_now = std::localtime(&now);
 
+    char buffer[32];
 
+    std::strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", local_now);
 
+    std::cout << "当前时间: " << buffer << std::endl;
 
+    return 0;
+}
 
+std::string filepath = "";
 
+void LogToFile() {
 
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+void Logger::Log(Level level, const std::string *message)
+{
+    LevelToString(level);
+}
--- utility/logger.h
+++ utility/logger.h
@@ -2,4 +2,41 @@
 
 #include <iostream>
 #include <fstream>
+#include <ctime>
 
+#define LOG_LEVEL 0
+
+class Logger
+{
+public:
+    enum Level
+    {
+        Debug,
+        Info,
+        Warning,
+        Error,
+        Fatal
+    };
+
+    static void Log(Level level, const std::string *message);
+
+private:
+    static std::string LevelToString(Level level)
+    {
+        switch (level)
+        {
+        case Level::Debug:
+            return "Debug";
+        case Level::Info:
+            return "Info";
+        case Level::Warning:
+            return "Warning";
+        case Level::Error:
+            return "Error";
+        case Level::Fatal:
+            return "Fatal";
+        default:
+            return "Unknown";
+        };
+    };
+};
