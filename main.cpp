c940b714d2c103ead37cd676d9fdce0443e1e26a
腐海 <2276456482@qq.com>
2024-07-13 19:09:19 -0700

    learn

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
-#include <windows.h>
-#include <tlhelp32.h>
-#include <vector>
 
+
+int main()
+{
+	int a = 10;
+	int *b = &a;
+
+	std::cout << "Value of a: " << a << std::endl;
+	std::cout << "Address of a: " << &a << std::endl;
+	std::cout << "Value of b: " << *b << std::endl;
+	std::cout << "Address of b: " << &b << std::endl;
+}
