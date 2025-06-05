dddba1f55f0194026b38bef686a2f0f61434a20b
腐海 <86948417+RottenSea@users.noreply.github.com>
2025-06-05 03:55:26 -0800

    Modify

---
+++ 2506052.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        printf("用法: %s <源文件> <目标文件>\n", argv[0]);
+        return 1;
+    }
+
+    FILE *src_file = fopen(argv[1], "rb");
+    if (src_file == NULL)
+    {
+        perror("无法打开源文件");
+        return 1;
+    }
+
+    FILE *dst_file = fopen(argv[2], "ab");
+    if (dst_file == NULL)
+    {
+        perror("无法打开目标文件");
+        fclose(src_file);
+        return 1;
+    }
+
+    int ch;
+    while ((ch = fgetc(src_file)) != EOF)
+    {
+        fputc(ch, dst_file);
+    }
+
+    fclose(src_file);
+    fclose(dst_file);
+    printf("文件内容追加成功！\n");
+    return 0;
+}
