bbcce1245824d6584f2a60d8a9cae809b4b27a09
腐海 <86948417+RottenSea@users.noreply.github.com>
2025-06-18 02:25:36 -0800

    initialize

--- Flight.cpp
+++ Flight.cpp
@@ -11,11 +11,19 @@
 #define MAX_PASSWORD_LENGTH 21
 #define MAX_PHONE_LENGTH 12
 
+const char *USER_FILE = "users.dat";
+
+void initialize();
+void load_users();
+void save_users();
+
 void reg();
 void login();
 void userMenu();
 void adminMenu();
 void enterMenu();
+
+void pressEnterToContinue();
 
 // 用户信息结构体
 typedef struct
@@ -36,7 +44,7 @@ typedef struct
     char UUID[MAX_UUID_LENGTH];
     char NAME[MAX_NAME_LENGTH];
     char PASSWORD[MAX_PASSWORD_LENGTH];
-    int ROLE = 0;
+    int ROLE = 0; // 0用户管理员 1航班管理员
 } ADMINISTRATOR;
 ADMINISTRATOR admins[MAX_ADMINISTRATOR_NUMBER];
 int adminsCount = 0;
@@ -81,6 +89,63 @@ int main()
             break;
         }
     } while (1);
+}
+
+/**
+ * 初始化数据
+ */
+void initialize()
+{
+    // 如果文件已经存在 跳过初始化
+    FILE *fp = fopen(USER_FILE, "r");
+    if (fp)
+    {
+        fclose(fp);
+        return;
+    }
+
+    usersCount = 0;
+    adminsCount = 0;
+    entersCount = 0;
+
+    // 添加示例用户
+    USER u1;
+    strcpy(u1.UUID, "001");
+    strcpy(u1.NAME, "001");
+    strcpy(u1.PASSWORD, "001");
+    strcpy(u1.PHONE, "13300001111");
+    u1.gender = 1;
+    u1.age = 18;
+    users[usersCount++] = u1;
+
+    ADMINISTRATOR u2;
+    strcpy(u1.UUID, "002");
+    strcpy(u1.NAME, "002");
+    strcpy(u1.PASSWORD, "002");
+    u2.ROLE = 0;
+    admins[adminsCount++] = u2;
+
+    strcpy(u1.UUID, "003");
+    strcpy(u1.NAME, "003");
+    strcpy(u1.PASSWORD, "003");
+    u2.ROLE = 1;
+    admins[adminsCount++] = u2;
+
+    ENTERPRISE u3;
+    strcpy(u1.UUID, "004");
+    strcpy(u1.NAME, "004");
+    strcpy(u1.PASSWORD, "004");
+    enters[entersCount] = u3;
+
+    save_users();
+}
+
+void load_users()
+{
+}
+
+void save_users()
+{
 }
 
 void reg()
@@ -184,4 +249,20 @@ void login()
     }
 
     printf("UUID不存在");
+}
+
+void userMenu()
+{
+}
+
+void adminMenu()
+{
+}
+
+void enterMenu()
+{
+}
+
+void pressEnterToContinue()
+{
 }
