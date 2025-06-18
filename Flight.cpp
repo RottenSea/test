ed142fc25a8ed081346f071449aa0c4fe60b6924
腐海 <86948417+RottenSea@users.noreply.github.com>
2025-06-18 04:33:25 -0800

    Sync

--- Flight.cpp
+++ Flight.cpp
@@ -1,21 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #define MAX_USER_NUMBER 100
 #define MAX_ADMINISTRATOR_NUMBER 100
 #define MAX_ENTERPRISE_NUMBER 100
+#define MAX_FLIGHT_NUMBER 100
 
 #define MAX_UUID_LENGTH 21
 #define MAX_NAME_LENGTH 21
 #define MAX_PASSWORD_LENGTH 21
 #define MAX_PHONE_LENGTH 12
+#define MAX_ID_LENGTH 31
 
-const char *USER_FILE = "users.dat";
+const char *USER_FILE = "Users.dat";
+const char *USER_FLIGHT_FILE = "UserFlight.dat";
+const char *FLIGHTS_FILE = "Flights.dat";
 
 void initialize();
-void load_users();
-void save_users();
+void loadUsers();
+void saveUsers();
+void loadUserFlight();
+void saveUserFlight();
+void loadFlights();
+void saveFlishts();
 
 void reg();
 void login();
@@ -32,8 +41,8 @@ typedef struct
     char NAME[MAX_NAME_LENGTH];
     char PASSWORD[MAX_PASSWORD_LENGTH];
     char PHONE[MAX_PHONE_LENGTH];
-    int gender = 0; // 0未知 1男 2女
-    int age = 0;
+    int gender; // 0未知 1男 2女
+    int age;
 } USER;
 USER users[MAX_USER_NUMBER];
 int usersCount = 0;
@@ -44,7 +53,7 @@ typedef struct
     char UUID[MAX_UUID_LENGTH];
     char NAME[MAX_NAME_LENGTH];
     char PASSWORD[MAX_PASSWORD_LENGTH];
-    int ROLE = 0; // 0用户管理员 1航班管理员
+    int ROLE; // 0用户管理员 1航班管理员
 } ADMINISTRATOR;
 ADMINISTRATOR admins[MAX_ADMINISTRATOR_NUMBER];
 int adminsCount = 0;
@@ -58,6 +67,24 @@ typedef struct
 } ENTERPRISE;
 ENTERPRISE enters[MAX_ENTERPRISE_NUMBER];
 int entersCount = 0;
+
+// 航班信息结构体
+typedef struct
+{
+    char ID[MAX_ID_LENGTH];
+    char MODEL[MAX_NAME_LENGTH];
+    char COMPANY[MAX_NAME_LENGTH];
+    time_t time;
+    time_t duration;
+    char GATE[MAX_NAME_LENGTH];
+    char STARTING[MAX_NAME_LENGTH];
+    char DESTINATION[MAX_NAME_LENGTH];
+    int praise;
+    int num;
+    int curr;
+} FLIGHT;
+FLIGHT flights[MAX_FLIGHT_NUMBER];
+int flightsCount = 0;
 
 int main()
 {
@@ -111,7 +138,7 @@ void initialize()
     // 添加示例用户
     USER u1;
     strcpy(u1.UUID, "001");
-    strcpy(u1.NAME, "001");
+    strcpy(u1.NAME, "用户001");
     strcpy(u1.PASSWORD, "001");
     strcpy(u1.PHONE, "13300001111");
     u1.gender = 1;
@@ -120,31 +147,72 @@ void initialize()
 
     ADMINISTRATOR u2;
     strcpy(u1.UUID, "002");
-    strcpy(u1.NAME, "002");
+    strcpy(u1.NAME, "用户管理员002");
     strcpy(u1.PASSWORD, "002");
     u2.ROLE = 0;
     admins[adminsCount++] = u2;
 
     strcpy(u1.UUID, "003");
-    strcpy(u1.NAME, "003");
+    strcpy(u1.NAME, "航班管理员003");
     strcpy(u1.PASSWORD, "003");
     u2.ROLE = 1;
     admins[adminsCount++] = u2;
 
     ENTERPRISE u3;
     strcpy(u1.UUID, "004");
-    strcpy(u1.NAME, "004");
+    strcpy(u1.NAME, "中国南方航空004");
     strcpy(u1.PASSWORD, "004");
-    enters[entersCount] = u3;
+    enters[entersCount++] = u3;
 
-    save_users();
+    saveUsers();
+
+    flightsCount = 0;
+
+    FLIGHT f1;
+    strcpy(f1.ID, "001");
+    strcpy(f1.MODEL, "波音737");
+    f1.time = time(NULL);
+    f1.duration = 14400;
+    strcpy(f1.GATE, "A01");
+    strcpy(f1.STARTING, "浙江");
+    strcpy(f1.DESTINATION, "长春");
+    f1.praise = 1000;
+    f1.num = 162;
+    f1.curr = 100;
+
+    saveFlishts();
 }
 
-void load_users()
+void loadUsers()
 {
 }
 
-void save_users()
+void saveUsers()
+{
+    FILE *fp = fopen(USER_FILE, "wb");
+    if (fp == NULL)
+    {
+        printf("保存用户时文件打开失败！\n");
+        return;
+    }
+    fwrite(&usersCount, sizeof(int), 1, fp);
+    fwrite(users, sizeof(USER), usersCount, fp);
+    fclose(fp);
+}
+
+void loadUserFlight()
+{
+}
+
+void saveUserFlight()
+{
+}
+
+void loadFlights()
+{
+}
+
+void saveFlishts()
 {
 }
 
@@ -186,7 +254,8 @@ void login()
                 else
                 {
                     printf("密码输入错误,是否重新输入\n");
-                    printf("0取消并返回上级目录 1重新输入密码\n");
+                    printf("0取消并返回上级目录\n");
+                    printf("1重新输入密码\n");
                     printf("选择:");
                     scanf("%d", &choice);
                 }
@@ -213,7 +282,8 @@ void login()
                 else
                 {
                     printf("密码输入错误,是否重新输入\n");
-                    printf("0取消并返回上级目录 1重新输入密码\n");
+                    printf("0取消并返回上级目录\n");
+                    printf("1重新输入密码\n");
                     printf("选择:");
                     scanf("%d", &choice);
                 }
@@ -240,7 +310,8 @@ void login()
                 else
                 {
                     printf("密码输入错误,是否重新输入\n");
-                    printf("0取消并返回上级目录 1重新输入密码\n");
+                    printf("0取消并返回上级目录\n");
+                    printf("1重新输入密码\n");
                     printf("选择:");
                     scanf("%d", &choice);
                 }
@@ -263,6 +334,11 @@ void enterMenu()
 {
 }
 
+/**
+ * 按回车键继续
+ */
 void pressEnterToContinue()
 {
+    printf("\n按回车键继续...");
+    getchar();
 }
