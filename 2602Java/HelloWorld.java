ba7ceea7af1b6aeceed182363b7754eb4fd5fb61
腐海 <86948417+RottenSea@users.noreply.github.com>
2026-08-26 08:34:56 +0000

    Update

---
+++ 2602Java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String args[]) {
+        int a = 100, b = 5, c = 200;
+        System.out.print(a);
+        System.out.print(b);
+        System.out.print(c);
+    }
+}
