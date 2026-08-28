68211f8851dbce689c3c4ad20f0193659e63b742
腐海 <86948417+RottenSea@users.noreply.github.com>
2026-08-28 14:15:16 +0000

    Update

---
+++ 2602Java/Input.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter radius: ");
+        double radius = input.nextDouble();
+
+        double area = radius * radius * Math.PI;
+        System.out.println("The area for the circle of radius " + radius + " is " + area);
+
+        input.close();
+    }
+}
