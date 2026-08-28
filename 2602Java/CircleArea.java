67ec0024ddcdd88656dfcbc089a823e7ea081323
腐海 <86948417+RottenSea@users.noreply.github.com>
2026-08-28 02:26:39 +0000

    Create CircleArea.java
    Update

---
+++ 2602Java/CircleArea.java
@@ -0,0 +1,8 @@
+public class CircleArea {
+    public static void main(String[] args) {
+        double radius = 20;
+        double area = radius * radius * Math.PI;
+
+        System.out.println("The area for the circle of radius " + radius + " is " + area);
+    }
+}
